f19061ac3f3dc26bb80dd8912ea34f9136fb84e5
Serhii Blazhko <s.blazhko.dev@gmail.com>
2025-09-24 11:27:07 +0200

    updated example app dependencies

--- example/ios/InbrainReactNativeExample/AppDelegate.mm
+++ example/ios/InbrainReactNativeExample/AppDelegate.mm
@@ -1,14 +1,14 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"InbrainReactNativeExample";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
+  self.dependencyProvider = [RCTAppDependencyProvider new];
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
